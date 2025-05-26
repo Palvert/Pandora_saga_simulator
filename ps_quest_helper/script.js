b3713c90b3ae26a443e26196435f26790c2c27d6
Palver <63277841+Palvert@users.noreply.github.com>
2025-05-26 22:39:57 +0700

    Update script.js

--- ps_quest_helper/script.js
+++ ps_quest_helper/script.js
@@ -63,8 +63,7 @@ function sortTable(columnIndex) {
 
 function open_descr(tElement) {
   const frame = document.getElementById("html_qpage");
-  frame.src = `https://palvert.github.io/Pandora_saga_tools/ps_quest_helper/qdata/${tElement.innerText}.html`
-  console.log(`opening description: ps_quest_helper/qdata/${tElement.innerText}.html`)
+  frame.src = `/ps_quest_helper/qdata/${tElement.innerText}.html`;
 }
 
 sortTable(2);
