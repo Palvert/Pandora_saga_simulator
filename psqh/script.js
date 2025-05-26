f8dd29c65594ef84481d0b3a1e28378450dcecc8
Palver <63277841+Palvert@users.noreply.github.com>
2025-05-26 23:14:25 +0700

    Update script.js

--- psqh/script.js
+++ psqh/script.js
@@ -63,7 +63,7 @@ function sortTable(columnIndex) {
 
 function open_descr(tElement) {
   const frame = document.getElementById("html_qpage");
-  frame.src = `/psqh/qdata/${tElement.innerText}.html`;
+  frame.src = `/qdata/${tElement.innerText}.html`;
 }
 
 sortTable(2);
